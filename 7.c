7cefebb2ac95a6c15d684e8e2f6e70e85ec7db45
CarloRuzzon <carloruzzon@gmail.com>
2017-04-21 18:42:55 +0200

    fatto

---
+++ 7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+  int num, a, c=0;
+  do{
+    printf("Inserire il numero:");
+    scanf("%d", &num);
+  }while(num<1);
+  while(num>0){
+    a=num%10;
+    if(a==7){
+      c++;
+    }
+    num/=10;
+  }
+  printf("Il 7 e' ripetuto %d volte\n", c);
+  return 0;
+}
