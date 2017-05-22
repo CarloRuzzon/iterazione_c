202ebc9930d7bc8367b91d8eb6974a39b143aa13
ALUNNO <alunno@debian15>
2017-05-22 09:24:06 +0200

    fatto

--- duemagg.c
+++ duemagg.c
@@ -1,21 +1,27 @@
 #include<stdio.h>
 int main(){
-  int max,c, maxx, a=0, b, d;  
+  int max,c, maxx, a=0, t;  
   printf("Inserire i numeri:");
-  scanf("%d", &b);
+  scanf("%d", &max);
   printf("Inserire i numeri:");
-  scanf("%d", &d);
+  scanf("%d", &maxx);
+  if(maxx>max){
+    t=maxx;
+    maxx=max;
+    max=t;
+  }
   while(a<8){
     printf("Inserire i numeri:");
     scanf("%d", &c);
-    if(c>max){
-      if(c>maxx){
-        maxx=c;
-      }
-      max=maxx;
+    if(c>=max){
+      maxx=max;
+      max=c;
+    }
+    else if(c>=maxx){
+      maxx=c;
     }
     a++;
   }
-    printf("I numeri maggiori %d e %d", max, maxx);
+    printf("I numeri maggiori %d e %d\n", max, maxx);
   return 0;
 }
