69a1d18978cb5c0a476e056c068daa89c2560e28
ALUNNO <alunno@debian15>
2017-05-22 08:31:23 +0200

    aggiustato

--- cerchio.c
+++ cerchio.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main(){
-  float r, c, s;
+  double c, s;
+  int r;
  do{
     printf("Inserire il raggio:");
-    scanf("%f", &r);
-  }while(r<1);
-  c=2*r*3.14;
+    scanf("%d", &r);
+  }while(r<0);
+  c=2.0*r*3.14;
   s=r*r*3.14;
   printf("La circonferenza e' %.2f, e la superficie e' %.2f\n", c, s);
   return 0;
