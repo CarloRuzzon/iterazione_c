1589d282a7580870032ecaef1e316a0b7d4dce60
CarloRuzzon <carloruzzon@gmail.com>
2017-05-25 10:32:07 +0200

    fatto

---
+++ binario.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int numb, numbu, numd, app, contatore=0, a=0, m=1;
+  do{
+    printf("Inserire il numero in binario:");
+    scanf("%d", &numb);
+    numbu=numb;
+    while(numbu>0){
+      app = numbu % 10;
+      numbu /= 10;
+      if(app==0 || app==1){
+        contatore++;
+        a=1;
+        
+      }
+      else{
+        printf("Reinserire il numero correttamente.\n");
+        break;
+      }
+    }
+    numbu=numb;
+  }while(a==0);
+  while(contatore>0){
+    app=numbu%10;
+    numbu/=10;
+    numd=numd+app*m;
+    m*=2;
+    contatore--;
+  }
+    printf("Il numero in binario e' %d, il numero in decimale e' %d\n", numb, numd);
+  return 0; 
+}
