dbbe0ba4dfdc9b581228ee9cb39303abea88585f
CarloRuzzon <carloruzzon@gmail.com>
2017-05-26 11:52:39 +0200

    aggiustato

--- binario.c
+++ binario.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-  int numb, numbu, numd, app, contatore=0, a=0, m=1;
+  int numb, numbu, numd=0, app, a=1, m=1;
   do{
     printf("Inserire il numero in binario:");
     scanf("%d", &numb);
@@ -9,25 +9,18 @@ int main(){
     while(numbu>0){
       app = numbu % 10;
       numbu /= 10;
-      if(app==0 || app==1){
-        contatore++;
-        a=1;
-        
-      }
-      else{
-        printf("Reinserire il numero correttamente.\n");
+      if(app!=0 && app!=1){
         a=0;
         break;
       }
     }
     numbu=numb;
   }while(a==0);
-  while(contatore>0){
+  while(numbu>0){
     app=numbu%10;
     numbu/=10;
     numd=numd+app*m;
     m*=2;
-    contatore--;
   }
     printf("Il numero in binario e' %d, il numero in decimale e' %d\n", numb, numd);
   return 0; 
---
+++ cripta.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+  int num;
+  do{
+  printf("Inserire un numero (di quattro cifre):");
+  scanf("%d", &num);
+  }while(num<10000 || num>
