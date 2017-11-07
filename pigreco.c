dbe40f7f02b833528ceb435a30bbf39636f42470
CarloRuzzon <carloruzzon@gmail.com>
2017-11-07 22:43:32 +0100

    fatto

--- pigreco.c
+++ pigreco.c
@@ -2,23 +2,31 @@
 
 int main ()
 {
-  float pi, b = 3;
-  int a, i;
+  float pi = 4., b = 3.;
+  int num, a;
   
   do {
     printf("Quanta precisione vuoi avere? ");
-    scanf("%d", &a);
-  } while (a < 1);
+    scanf("%d", &num);
+  } while (num < 1);
 
-  pi = 4 - 4 / b;
-  
-  for (i = 1; i < a - 1; i++) {
-    b += 2;
-    pi += 4 / b;
-    b += 2;
-    pi -= 4/b;
+  for(a = 1; a < num; a++){
+	if(num>1){
+	  pi = pi - 4. / b;
+	  num--;
+	  b+=2;
+	}
+	if(num > 1){
+	  pi = pi + 4. / b;
+	  num--;
+	  b+=2;
+	}
   }
 
   printf("%f", pi);
+
+
+
+  
   return 0;
 }
